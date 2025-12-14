fccdd70ede6e249436fa324809e9474b83bbf29d
ayoub-jabiri1 <ayoubjabiri.coding@gmail.com>
2025-12-14 21:18:36 +0100

    Added the dynamic background image functionality

--- main.js
+++ main.js
@@ -15,9 +15,8 @@ const getData = async (city) => {
         `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=25c66ab9631c6269c272a585437cc3ff&units=metric`
     );
 
+    // Handle 404 error
     if (!res.ok) {
-        console.log(res);
-
         appBody.innerHTML = `
             <div id="error-msg">
                 <h2>City not found!</h2>
@@ -29,10 +28,15 @@ const getData = async (city) => {
 
     const data = await res.json();
 
+    // Set app background image
+    handleAppBackground(city);
+
+    // Set data in the page
     appBody.innerHTML = `
         <h2 id="temp">${data.main.temp}°C</h2>
         <span id="city">${data.name}</span>
         <span id="condition">${data.weather[0].description}</span>
+        <img id="condition-icon" src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png">
         <div class="wind-and-humidity">
             <div class="humidity-box">
                 <i class="ri-water-percent-line icon"></i>
@@ -53,10 +57,24 @@ const getData = async (city) => {
     `;
 };
 
+const handleAppBackground = async (query) => {
+    const res = await fetch(
+        `https://api.unsplash.com/search/photos?page=1&query=${query}&client_id=_Ah4AchsiC5I3Ld-w0xM9D6ZixG_fW0ud2xmfr19imI`
+    );
+
+    const data = await res.json();
+
+    // Set the fetched image as the background image
+    document.getElementById(
+        "app"
+    ).style.backgroundImage = `url("${data.results[0].urls.regular}")`;
+};
+
 const handleTheme = () => {
     let rootElement = document.documentElement,
         logo = document.querySelector(".logo img");
 
+    // Toggle the global css variables
     if (theme == "light") {
         rootElement.style.setProperty("--main-container-color", "#d9d9d980");
         rootElement.style.setProperty(
@@ -96,5 +114,3 @@ searchBtn.addEventListener("click", () => {
 });
 
 themeBtn.addEventListener("click", handleTheme);
-
-// https://api.unsplash.com/photos/?client_id=_Ah4AchsiC5I3Ld-w0xM9D6ZixG_fW0ud2xmfr19imI
